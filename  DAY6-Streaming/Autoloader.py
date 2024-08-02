bead5087c74c835113a4dc4adc7f39dcafeb9d8a
thommichen <thommichen>
2024-08-02 04:40:26 +0000

    DAY6

---
+++  DAY6-Streaming/Autoloader.py
@@ -0,0 +1,34 @@
+# Databricks notebook source
+(spark
+.readStream
+.format("cloudFiles")
+.option("cloudFiles.format","csv")
+.option("cloudFiles.inferColumnTypes",True)
+.option("cloudFiles.schemaLocation","dbfs:/mnt/hexawaredatabricks/raw/schemalocation/Stephen/autoloader")
+.load("dbfs:/mnt/hexawaredatabricks/raw/stream_in/")
+.writeStream
+.option("checkpointLocation","dbfs:/mnt/hexawaredatabricks/raw/checkpoint/Stephen/autoloader")
+.table("spdatabricksws.bronze.autoloader")
+)
+
+# COMMAND ----------
+
+# DBTITLE 1,Final Autoloader Script
+(spark
+.readStream
+.format("cloudFiles")
+.option("cloudFiles.format","csv")
+.option("cloudFiles.inferColumnTypes",True)
+.option("cloudFiles.schemaEvolutionMode","rescue")
+.option("cloudFiles.schemaLocation","dbfs:/mnt/hexawaredatabricks/raw/schemalocation/Stephen/autoloader")
+.load("dbfs:/mnt/hexawaredatabricks/raw/stream_in/")
+.writeStream
+.option("checkpointLocation","dbfs:/mnt/hexawaredatabricks/raw/checkpoint/Stephen/autoloader")
+.option("mergeSchema",True)
+.table("spdatabricksws.bronze.autoloader")
+)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from bronze.autoloader
